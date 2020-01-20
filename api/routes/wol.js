16c84749dd9741683b65b8412c539392be14ab10
MatRusTy <mathiasp9999@gmail.com>
2020-01-20 19:20:10 +0100

    The WOL feature is now done. When deploying, insert the actual sh-command

--- api/routes/wol.js
+++ api/routes/wol.js
@@ -1,34 +1,51 @@
 const express = require('express');
 const router = express.Router();
-
-const devices = [{name:'RusTy-PC', status: 'Offline'}, {name:'RusTy-TV', status: 'Offline'}, {name:'RusTy-Laptop', status: 'Offline'}];
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const devicesText = fs.readFileSync('./config/devices.json');
+const devices = JSON.parse(devicesText).devices; //Array with all devices
+const devicesStatus = initDevices();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "fetching all added WOL devices",
-        devices: devices
+        devices: devicesStatus
     });
 });
 
 router.post('/', (req, res, next) => {
-    const wakeReq = {
-        device: req.body.device
-    }
-    const exists = devices.includes(wakeReq.device);
+    const deviceName = req.body.deviceName;
+    const exists = deviceExists(deviceName);
     if(exists) {
         //Wake the device:
-        //TODO: run shell script
-        // const execSync = require('child_process').execSync;
-        // const output = execSync('YOUR COMMAND HERE', { encoding: 'utf-8' });  // the default is 'buffer'
-        // console.log('Output was:\n', output);
+        const device = devices.find(device => {
+            return (device.deviceName === deviceName);
+        })
+        console.log(device);
+        const macAddress = device.deviceMacAddress;
+        const output = execSync('dir', { encoding: 'utf-8' }); // replace "dir" with etherwake command + macAddress
+        console.log('Output was:\n', output); // TODO: Add to log instead of printing in console
         res.status(201).json({
-            message: 'Device: ' + wakeReq.device + ' was woken'
+            message: 'Device: ' + deviceName + ' was woken'
         });
     } else {
         res.status(404).json({
-            message: 'Error: could not find device: ' + wakeReq.device
+            message: 'Error: could not find device: ' + deviceName
         });
     }
 });
+
+function deviceExists(deviceName) {
+    const deviceNames = [];
+    devices.forEach(device => {
+        deviceNames.push(device.deviceName);
+    });
+    return deviceNames.includes(deviceName);
+}
+
+function initDevices() {
+    return [{deviceName:'RusTy-PC', status: 'Offline'}, {deviceName:'RusTy-TV', status: 'Offline'}, {deviceName:'RusTy-Laptop', status: 'Offline'}];
+}
+
 
 module.exports = router;
---
+++ config/readme.md
@@ -0,0 +1,6 @@
+A 'devices.json' file is needed in this directory.
+It should contain a list of all devices of the form:
+{
+    deviceName = "name"
+    deviceMacAddress = "macaddress"
+}
